c0555d9fb30b58716ce70a4c5ba750fe03f340e2
caj52 <jarnaginconnor52@gmail.com>
2021-10-25 21:56:48 -0600

    -Created perlin maps editor extension

--- Assets/Script/PerlinClasses/PerlinArrays.cs
+++ Assets/Script/PerlinClasses/PerlinArrays.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class PerlinArrays : MonoBehaviour
 {//////////////////////////////////height&width,scale,coordx,coordy,frequency,lacunarity,persistance,octaves,amplitude
     private const float UnitCoordinatesBase = 10;
-    private static float[] _heightMapArray =       { 0, .3f, 10, 10, 0.1f,   3,    .7f,  2 ,1};
-    private static float[] _heightIntensityArray = { 0, .2f, 10, 10, 0.1f, 1.15f, 1.25f, 4 ,1};
-    private static float[] _zonesArray =           { 0, .1f, 10, 10, 0.01f,  2f,   1f,   6 ,0};
+    private static float[] _heightMapArray =       { 200, .3f, 10, 10, 0.1f,   3,    .7f,  2 ,1};
+    private static float[] _heightIntensityArray = { 200, .2f, 10, 10, 0.1f, 1.15f, 1.25f, 4 ,1};
+    private static float[] _zonesArray =           { 200, .1f, 10, 10, 0.01f,  2f,   1f,   6 ,0};
     public static float[] GetHeightMapArray()
     {   
         return _heightMapArray;   
--- Assets/Script/PerlinClasses/PerlinGen.cs
+++ Assets/Script/PerlinClasses/PerlinGen.cs
@@ -52,7 +52,8 @@ public class PerlinGen : MonoBehaviour
            }
             frequency *= lacunarity;
             amplitude *= persistence;
-        }
+           
+       }
        return map;
     }
 
