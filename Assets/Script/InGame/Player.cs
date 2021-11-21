b753ea4a26760571532c918259a0c264c7d8a7f0
caj52 <jarnaginconnor52@gmail.com>
2021-11-20 19:30:30 -0700

    -Added Player UI to the main scene. Non Functional currently

--- Assets/Script/InGame/Player.cs
+++ Assets/Script/InGame/Player.cs
@@ -19,41 +19,43 @@ public class Player : MonoBehaviour
 
     private void HandleInput()
     {
-        var w = Input.GetKey(KeyCode.W);
-        var a = Input.GetKey(KeyCode.A);
-        var s = Input.GetKey(KeyCode.S);
-        var d = Input.GetKey(KeyCode.D);
+        var wDown = Input.GetKey(KeyCode.W);
+        var aDown = Input.GetKey(KeyCode.A);
+        var sDown = Input.GetKey(KeyCode.S);
+        var dDown = Input.GetKey(KeyCode.D);
+        var qDown = Input.GetKey(KeyCode.Q);
+        var eDown = Input.GetKey(KeyCode.E);
         var wUp = Input.GetKeyUp(KeyCode.W);
         var aUp = Input.GetKeyUp(KeyCode.A);
         var sUp = Input.GetKeyUp(KeyCode.S);
         var dUp = Input.GetKeyUp(KeyCode.D);
         var moved = false;
-        if (w)
+        if (wDown)
         {
             creature.Move(transform.forward);
             moved = true;
         }
-        if (s)
+        if (sDown)
         {
             creature.Move(-transform.forward);
             moved = true;
         }
-        if (d)
+        if (dDown)
         {
             creature.Move(transform.right);
             moved = true;
         }
-        if (a)
+        if (aDown)
         {
             creature.Move(-transform.right);
             moved = true;
         }
         if(wUp||aUp||sUp||dUp)
             creature.StopMoveImmediate();
-        if(Input.GetMouseButton(1) && Input.mousePosition.x>Screen.width-(Screen.width/4))
-            transform.Rotate(0.0f, 5.0f, 0.0f, Space.Self);
-        if(Input.GetMouseButton(1) && Input.mousePosition.x<Screen.width/4)
-            transform.Rotate(0.0f, -5.0f, 0.0f, Space.Self);
+        if(eDown)
+            transform.Rotate(0.0f, 2.0f, 0.0f, Space.Self);
+        if(qDown)
+            transform.Rotate(0.0f, -2.0f, 0.0f, Space.Self);
         if(moved)
             UpdateWorldWindowPosition();
     }
